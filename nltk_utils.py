85cb8679850efc63e643faa620c3d8d080e137b8
Omkar Yadav <oy970831@gmail.com>
2021-06-24 12:18:55 +0530

    requrements

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,3 +1,7 @@
 import nltk
 nltk.download('all-corpora')
 nltk.download('vader_lexicon')
+
+import subprocess
+cmd = ['pip3','install','--user','--upgrade','git+https://github.com/twintproject/twint.git@origin/master#egg=twint']
+subprocess.run(cmd)
