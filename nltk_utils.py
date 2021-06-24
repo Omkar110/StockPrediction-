d84f3da513499fb0ba6fd7b4be3c58b8bd519d01
Omkar Yadav <oy970831@gmail.com>
2021-06-24 12:36:26 +0530

    dp6

--- nltk_utils.py
+++ nltk_utils.py
@@ -5,3 +5,4 @@ nltk.download('vader_lexicon')
 import subprocess
 cmd = ['pip3','install','--user','--upgrade','git+https://github.com/twintproject/twint.git@origin/master#egg=twint']
 subprocess.run(cmd)
+print("working")
